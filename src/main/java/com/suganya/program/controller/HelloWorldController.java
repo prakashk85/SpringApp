5ae0c28c81fc672f8fa7ae28cd72244f9f1c3e16
suganya <miss.suganya@gmail.com>
2018-04-30 22:06:17 +0530

    second commit

--- src/main/java/com/suganya/program/controller/HelloWorldController.java
+++ src/main/java/com/suganya/program/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.suganya.program.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,14 +10,25 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloWorldController {
 	String message = "Welcome to Spring MVC!";
 
-	@RequestMapping("/hello")
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
-
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
 		return mv;
+	}
+
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	public ModelAndView showMessage1(@RequestParam(value = "count", required = true, defaultValue = "1") int count,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name+calc(count));
+		return mv;
+	}
+	
+	private int calc (int c){
+		return c*c;
 	}
 }
